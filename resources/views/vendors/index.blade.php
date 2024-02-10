0a2c0120637fe3415a8e1f2cbc3d6a2ba97a72fe
n0w3ll <noel.dev2023@gmail.com>
2024-02-10 12:42:01 +0800

    update colors

--- resources/views/vendors/index.blade.php
+++ resources/views/vendors/index.blade.php
@@ -5,21 +5,21 @@
     </h2>
 
     <div class="flex justify-between items-center mt-6">
-      <div class="relative w-full max-w-xl mr-6 focus-within:text-purple-500">
+      <div class="relative w-full max-w-xl mr-6 focus-within:text-blue-500">
         <div class="absolute inset-y-0 flex items-center pl-2">
           <svg class="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
             <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
           </svg>
         </div>
         <form action="{{ route('vendors.index') }}" method="GET">
-          <input class="w-full pl-8 pr-2 text-sm text-gray-700 placeholder-gray-600 bg-gray-100 border-0 rounded-md dark:placeholder-gray-500 dark:focus:shadow-outline-gray dark:focus:placeholder-gray-600 dark:bg-gray-700 dark:text-gray-200 focus:placeholder-gray-500 focus:bg-white focus:border-purple-300 focus:outline-none focus:shadow-outline-purple form-input" type="text" name="q" value="{{ $searched }}" placeholder="Search for vendor" aria-label="Search">
+          <input class="w-full pl-8 pr-2 text-sm text-gray-700 placeholder-gray-600 bg-gray-100 border-0 rounded-md dark:placeholder-gray-500 dark:focus:shadow-outline-gray dark:focus:placeholder-gray-600 dark:bg-gray-700 dark:text-gray-200 focus:placeholder-gray-500 focus:bg-white focus:border-blue-300 focus:outline-none focus:shadow-outline-blue form-input" type="text" name="q" value="{{ $searched }}" placeholder="Search for vendor" aria-label="Search">
         </form>
       </div>
       <div class="space-x-4">
 
         @unless ($searched == "")
         <a class="text-right" href="{{ route('vendors.index') }}">
-          <button class="inline-flex items-center px-4 py-2 bg-purple-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-purple-700 focus:bg-purple-700 active:bg-purple-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+          <button class="inline-flex items-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 focus:bg-blue-700 active:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
             < ALL VENDORS</button>
         </a>
         @endunless
